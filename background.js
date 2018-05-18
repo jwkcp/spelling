cda884955bb54d43648b0195b7f8b008df8017be
jaewoong.go@gmail.com <jaewoong.go@gmail.com>
2018-05-18 17:04:16 +0900

    1. 맞춤법 검사 엔진을 네이버에서 사람인으로 교체함. 2. 사람인 관련 안내 및 기부 안내 문구 추가 및 수정

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 function executeInjectedScript(callback) {
+    // 원래 코드 부분
     chrome.tabs.executeScript(null, {file: 'contents.js'});
     chrome.runtime.onMessage.addListener(function(msg) {
         callback(msg);
--- popup.js
+++ popup.js
@@ -1,46 +1,113 @@
+const NAVER_URL = "https://m.search.naver.com/p/csearch/ocontent/spellchecker.nhn?_callback=window.__jindo2_callback._spellingCheck_0&q=";
+const SARAMIN_URL = "http://www.saramin.co.kr/zf_user/tools/spell-check?content=";
+
+// 맞춤법 검사기 클래스
+var spellChecker = {
+    url : null,
+    parse : function() {
+        return {
+            text : null,
+            count : -1,
+        };
+    }
+}
+
+// 네이버 파싱
+function parseForNaver(resp) {
+    json_text = resp.replace("window.__jindo2_callback._spellingCheck_0(", "").replace(");", "");
+    json_obj = JSON.parse(json_text);
+    
+    return {
+        text : json_obj.message.result.html,
+        count : json_obj.message.result.errata_count
+    };
+};
+
+// Javascript의 replace는 첫 문자열만 교체하기 때문에 모든 문자열 교체를 위해 작성함.
+function replaceAll(str, old_word, new_word) {
+    return str.split(old_word).join(new_word);
+}
+
+// 사람인 파싱
+function parseForSaramin(resp) {
+    json_text = resp;
+    json_obj = JSON.parse(json_text);
+    
+    result_with_replace = json_obj.result_text;
+    
+    // 결과 문자열 교체
+    for (var i in json_obj.word_list) {
+        result_with_replace = replaceAll(result_with_replace, json_obj.word_list[i].errorWord, json_obj.word_list[i].candWordList[0])
+    }
+    
+    return {
+        text : result_with_replace,
+        count : json_obj.result_cnt
+    };
+};
+
 $(document).ready(function() {
-    function spellChecker(origin_text, callback) {
-        var url = 'https://m.search.naver.com/p/csearch/ocontent/spellchecker.nhn?_callback=window.__jindo2_callback._spellingCheck_0&q=';
-        var client = new XMLHttpRequest();
-        var json_text = '';
-        var json_obj = '';
 
-        url += origin_text;
-
+    var keepResultHtml;
+    var keepResultText;
+    
+    var sc = Object.create(spellChecker);
+    
+    var client = new XMLHttpRequest();
+    function run(origin_text, callback) {
         client.onreadystatechange = function () {
             if (client.readyState === XMLHttpRequest.DONE && client.status === 200) {
-                json_text = client.responseText.replace("window.__jindo2_callback._spellingCheck_0(", "").replace(");", "");
-                json_obj = JSON.parse(json_text);
-
+                
+                var results = sc.parse(client.responseText);
+                
                 if ("function" === typeof callback) {
                     callback(
-                        json_obj.message.result.html,
-                        json_obj.message.result.errata_count
+                        results.text,
+                        results.count
                     );
                 }
             }
         }
-
-        client.open("GET", url, true);
-        // 2018.05.11 referer 체크 추가
-        client.setRequestHeader('Access-Control-Allow-Origin', '*');
-        client.setRequestHeader('Referer', 'https://search.naver.com/search.naver?ie=UTF-8&query=%EB%84%A4%EC%9D%B4%EB%B2%84%EB%A7%9E%EC%B6%A4%EB%B2%95&sm=chr_hty');
-        // ...
+        
+        sc.parse = parseForSaramin;
+        sc.url = SARAMIN_URL + origin_text;
+        
+        client.open("GET", sc.url, true);
         client.send(null);
     }
+    
 
-    var keepResultHtml;
-    var keepResultText;
-
-    $("#check-fs-link").click(function () {
-        if (keepResultText) {
-            window.open("popup_fs.html", "popup_window", "width=" + screen.availWidth + ", height=" + screen.availHeight + ", scrollbars=no");
+    $("#check-button").click(function() {
+        // 입력 값 획득
+        var origin_text = $("#check-target").val();
+        var $check_result_label = $('#check-result-label');
+        
+        // 입력 값이 공백일 경우 처리
+        if ("" == $.trim(origin_text)) {
+            $('#check-result').text("");
+            $check_result_label.removeClass("label-default label-success label-danger").addClass("label-default");
+            $check_result_label.text("결과");
+            return;
         }
-        else {
-            $("#check-fs-link").text("맞춤법 검사를 먼저 해야 해요!");
-        }
+        
+        // 맞춤법 검사
+        run(origin_text, function(result_html, error_count) {
+            $('#check-result').html(result_html);
+        
+            if (0 < error_count) {
+                $check_result_label.removeClass("label-default label-success label-danger").addClass("label-danger");
+                $check_result_label.text("맞춤법이 틀렸습니다 (" + error_count + ")");
+            }
+            else {
+                $check_result_label.removeClass("label-default label-success label-danger").addClass("label-success");
+                $check_result_label.text("맞춤법이 정확합니다");
+            }
+        
+            keepResultHtml = result_html;
+            keepResultText = $('#check-result').text();
+        });
     });
-
+    
     $('#check-result').mouseenter(function () {
         if (keepResultText) {
             $('#check-result').html(keepResultText);
@@ -53,42 +120,11 @@ $(document).ready(function() {
         }
     });
 
-    $("#check-button").click(function() {
-
-        var origin_text = $("#check-target").val();
-        var $check_result_label = $('#check-result-label');
-
-        if ("" == $.trim(origin_text)) {
-            $('#check-result').text("");
-            $check_result_label.removeClass("label-default label-success label-danger").addClass("label-default");
-            $check_result_label.text("결과");
-            return;
-        }
-
-        spellChecker(origin_text, function(result_html, error_count) {
-            $('#check-result').html(result_html);
-
-            if (0 < error_count) {
-                $check_result_label.removeClass("label-default label-success label-danger").addClass("label-danger");
-                $check_result_label.text("맞춤법이 틀렸습니다 (" + error_count + ")");
-            }
-            else {
-                $check_result_label.removeClass("label-default label-success label-danger").addClass("label-success");
-                $check_result_label.text("맞춤법이 정확합니다");
-            }
-
-            keepResultHtml = result_html;
-            keepResultText = $('#check-result').text();
-        });
-    });
-
     $("#check-target").keypress(function(e) {
         if (e.keyCode == 13) {
             $("#check-button").click();
         }
     });
-
-
 
     chrome.runtime.getBackgroundPage(function(bgPage) {
         bgPage.executeInjectedScript(function(result) {
@@ -103,5 +139,14 @@ $(document).ready(function() {
             $("#check-target").text(result.selText);
             $("#check-button").click();
         });
+    });
+    
+    $("#check-fs-link").click(function () {
+        if (keepResultText) {
+            window.open("popup_fs.html", "popup_window", "width=" + screen.availWidth + ", height=" + screen.availHeight + ", scrollbars=no");
+        }
+        else {
+            $("#check-fs-link").text("맞춤법 검사를 먼저 해야 해요!");
+        }
     });
 });
