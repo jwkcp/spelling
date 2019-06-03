7722c8f9497fc6d69a24520e7b088503148ac644
jaewoong <jaewoong.go@gmail.com>
2019-06-03 17:17:14 +0900

    검사 결과 응답 json 변경으로 인한 버그 수정. 맞춤법 도움 메시지 추가. 기부 카카오페이 QR추가

--- popup.js
+++ popup.js
@@ -8,6 +8,7 @@ var spellChecker = {
         return {
             text : null,
             count : -1,
+            msg: ""
         };
     }
 }
@@ -34,15 +35,46 @@ function parseForSaramin(resp) {
     json_obj = JSON.parse(json_text);
     
     result_with_replace = json_obj.result_text;
-    
-    // 결과 문자열 교체
+
+    let errorWord = null
+    let candWord = null
+    let msg = ""
+
     for (var i in json_obj.word_list) {
-        result_with_replace = replaceAll(result_with_replace, json_obj.word_list[i].errorWord, json_obj.word_list[i].candWordList[0])
+        /* 결과 문자열 교체 */    
+        
+        // 2019.06.03 Tony
+        // candWordList가 과거 리스트형태로 제공되었으나 갑자기 문자열로 바뀐 것을 확인함. 
+        // 하지만 모든 결과가 문자열인지 확신할 수 없고 명칭 역시 candWordList이므로 타입 검사를 하는 로직을 추가함
+        errorWord = json_obj.word_list[i].errorWord
+        candWord = json_obj.word_list[i].candWordList
+        // 맞춤법 도움 메시지도 추가함.
+        msg = json_obj.word_list[i].helpMessage
+        if (msg) {
+            msg = msg.trim().split("\n")
+
+            if (msg.length > 0) {
+                msg = msg[0]
+            } else {
+                msg = ""
+            }
+        }
+
+        if (typeof candWord === "object") {
+            if (candWord.length > 0) {
+                candWord = json_obj.word_list[i].candWordList[0]
+            } else {
+                candWord = ""
+            }
+        }   
+
+        result_with_replace = replaceAll(result_with_replace, errorWord, candWord)
     }
     
     return {
         text : result_with_replace,
-        count : json_obj.result_cnt
+        count : json_obj.result_cnt,
+        msg: msg
     };
 };
 
@@ -63,7 +95,8 @@ $(document).ready(function() {
                 if ("function" === typeof callback) {
                     callback(
                         results.text,
-                        results.count
+                        results.count,
+                        results.msg
                     );
                 }
             }
@@ -85,14 +118,16 @@ $(document).ready(function() {
         // 입력 값이 공백일 경우 처리
         if ("" == $.trim(origin_text)) {
             $('#check-result').text("");
+            $('#check-result-msg').text("");
             $check_result_label.removeClass("badge-secondary badge-success badge-danger").addClass("badge-secondary");
             $check_result_label.text("결과");
             return;
         }
         
         // 맞춤법 검사
-        run(origin_text, function(result_html, error_count) {
+        run(origin_text, function(result_html, error_count, help_message) {
             $('#check-result').html(result_html);
+            $('#check-result-msg').html(help_message);
         
             if (0 < error_count) {
                 $check_result_label.removeClass("badge-secondary badge-success badge-danger").addClass("badge-danger");
